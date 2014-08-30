5f07ed2121c99892e1fb135f2410d5cdb34f2291
PhP Centre <barthchuk@yahoo.com>
2014-08-30 17:23:46 +0100

    Update restore_factory_settings.php

--- lib/restore_factory_settings.php
+++ lib/restore_factory_settings.php
@@ -1,8 +1,8 @@
 <?php
   global $wpdb;
-  $sql = "TRUNCATE TABLE " . jssor_sliders();
+  $sql = "TRUNCATE TABLE " .JssorSliderPlugin::jssor_sliders();
   $wpdb->query($sql);
 
-  $sql = "TRUNCATE TABLE " . jssor_slides();
+  $sql = "TRUNCATE TABLE " .JssorSliderPlugin::jssor_slides();
   $wpdb->query($sql);
 ?>
