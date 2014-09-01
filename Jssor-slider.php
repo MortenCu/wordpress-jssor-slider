7a6f4b0b5465ca6b3937c2f66bf3d027cfceb30b
PhP Centre <barthchuk@yahoo.com>
2014-09-01 16:10:56 +0100

    Update Jssor-slider.php

--- Jssor-slider.php
+++ Jssor-slider.php
@@ -18,22 +18,22 @@
 		exit; // disable direct access
 	}
 
-	if( !defined( 'JSSORSLIDER_VERSION' ) ) 		define( 'JSSORSLIDER_VERSION', '1.0' );
+	if( !defined( 'JSSORSLIDER_VERSION' ) ) define( 'JSSORSLIDER_VERSION', '1.0' );
 
-	if ( !defined( 'JSSOR_CONTENT_DIR' ) ) 			define( 'JSSOR_CONTENT_DIR', ABSPATH . 'wp-content' );
-	if ( !defined( 'JSSOR_MAIN_DIR' ) ) 			define( 'JSSOR_MAIN_DIR', ABSPATH . 'wp-content/jssor-slider' );
-	if ( !defined( 'JSSOR_MAIN_UPLOAD_DIR' ) ) 		define( 'JSSOR_MAIN_UPLOAD_DIR', ABSPATH . 'wp-content/jssor-slider/jssor-uploads/' );
-	if ( !defined( 'JSSOR_MAIN_THUMB_DIR' ) ) 		define( 'JSSOR_MAIN_THUMB_DIR', ABSPATH . 'wp-content/jssor-slider/thumbs/' );
-	if ( !defined( 'JSSOR_CONTENT_URL' ) ) 			define( 'JSSOR_CONTENT_URL', site_url() . '/wp-content' );
-	if ( !defined( 'JSSOR_PLUGIN_DIR' ) ) 			define( 'JSSOR_PLUGIN_DIR', JSSOR_CONTENT_DIR . '/plugins' );
-	if ( !defined( 'JSSOR_PLUGIN_URL' ) ) 			define( 'JSSOR_PLUGIN_URL', JSSOR_CONTENT_URL . '/plugins' );
-	if ( !defined( 'JSSOR_SL_PLUGIN_FILENAME' ) )	define( 'JSSOR_SL_PLUGIN_FILENAME', basename(__FILE__) );
-	if ( !defined( 'JSSOR_SL_PLUGIN_DIRNAME' ) ) 	define( 'JSSOR_SL_PLUGIN_DIRNAME', plugin_basename( dirname(__FILE__) ) );
-	if ( !defined( 'JSSOR_SL_PLUGIN_DIR' ) ) 		define( 'JSSOR_SL_PLUGIN_DIR', JSSOR_PLUGIN_DIR . '/' . JSSOR_SL_PLUGIN_DIRNAME );
-	if ( !defined( 'JSSOR_SL_PLUGIN_URL' ) ) 		define( 'JSSOR_SL_PLUGIN_URL', site_url() . '/wp-content/plugins/' . JSSOR_SL_PLUGIN_DIRNAME );
+	if ( !defined( 'JSSOR_CONTENT_DIR' ) ) define( 'JSSOR_CONTENT_DIR', ABSPATH . 'wp-content' );
+	if ( !defined( 'JSSOR_MAIN_DIR' ) ) define( 'JSSOR_MAIN_DIR', ABSPATH . 'wp-content/jssor-slider' );
+	if ( !defined( 'JSSOR_MAIN_UPLOAD_DIR' ) ) define( 'JSSOR_MAIN_UPLOAD_DIR', ABSPATH . 'wp-content/jssor-slider/jssor-uploads/' );
+	if ( !defined( 'JSSOR_MAIN_THUMB_DIR' ) ) define( 'JSSOR_MAIN_THUMB_DIR', ABSPATH . 'wp-content/jssor-slider/thumbs/' );
+	if ( !defined( 'JSSOR_CONTENT_URL' ) )  define( 'JSSOR_CONTENT_URL', site_url() . '/wp-content' );
+	if ( !defined( 'JSSOR_PLUGIN_DIR' ) ) define( 'JSSOR_PLUGIN_DIR', JSSOR_CONTENT_DIR . '/plugins' );
+	if ( !defined( 'JSSOR_PLUGIN_URL' ) ) define( 'JSSOR_PLUGIN_URL', JSSOR_CONTENT_URL . '/plugins' );
+	if ( !defined( 'JSSOR_SL_PLUGIN_FILENAME' ) ) define( 'JSSOR_SL_PLUGIN_FILENAME', basename(__FILE__) );
+	if ( !defined( 'JSSOR_SL_PLUGIN_DIRNAME' ) ) define( 'JSSOR_SL_PLUGIN_DIRNAME', plugin_basename( dirname(__FILE__) ) );
+	if ( !defined( 'JSSOR_SL_PLUGIN_DIR' ) ) define( 'JSSOR_SL_PLUGIN_DIR', JSSOR_PLUGIN_DIR . '/' . JSSOR_SL_PLUGIN_DIRNAME );
+	if ( !defined( 'JSSOR_SL_PLUGIN_URL' ) ) define( 'JSSOR_SL_PLUGIN_URL', site_url() . '/wp-content/plugins/' . JSSOR_SL_PLUGIN_DIRNAME );
 
-	if ( !defined( 'JSSOR_SL_THUMB_URL' ) ) 		define( 'JSSOR_SL_THUMB_URL', site_url() . '/wp-content/jssor-slider/jssor-uploads/' );
-	if ( !defined( 'JSSOR_SL_THUMB_SMALL_URL') ) 	define( 'JSSOR_SL_THUMB_SMALL_URL', site_url() . '/wp-content/jssor-slider/thumbs/' );
+	if ( !defined( 'JSSOR_SL_THUMB_URL' ) ) define( 'JSSOR_SL_THUMB_URL', site_url() . '/wp-content/jssor-slider/jssor-uploads/' );
+	if ( !defined( 'JSSOR_SL_THUMB_SMALL_URL') ) define( 'JSSOR_SL_THUMB_SMALL_URL', site_url() . '/wp-content/jssor-slider/thumbs/' );
 
 
 
