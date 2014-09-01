4fddbd586497d8b95bedec4159ca2da1e1242dd5
PhP Centre <barthchuk@yahoo.com>
2014-09-01 14:48:34 +0100

    Update jssor-slider-class.php

--- lib/jssor-slider-class.php
+++ lib/jssor-slider-class.php
@@ -1,68 +1,74 @@
 <?php	
 /**
-*@desc The core integration with WordPress.
-*	
-*/
+ *@desc The core integration with WordPress.
+ *	
+ */
 	
 class JssorSliderPlugin {	
 
 	/**
-	*singelton instance
-	*/	
+	 *singelton instance
+	 */	
 	private	static	$__instance;
 	
 	/**
-	* static entry/initialize singleton instance	
-	*/		
+	 * static entry/initialize singleton instance	
+	 */		
 	public static function run() {
+	
 		JssorSliderPlugin::getInstance();
+		
 	}
 	
 	/**
-	* get singelton	instance
-	*/	
+	 * get singelton	instance
+	 */	
 	public static function getInstance() {
 	
 		if( self::$__instance == null ) {
 			self::$__instance = new	self();
 		}
 		return	self::$__instance;
+		
 	}
 	
 	/**
-	* Constructor
-	*/
+	 * Constructor
+	 */
 	public function __construct() {
 	
 		$this->setup_actions();
 		$this->setup_shortcode();
 		$this->register_ajax_calls();
+		
 	}
 	
 	
 	/**
-	*Register the [jssorslider] shortcode.
-	*/
+	 *Register the [jssorslider] shortcode.
+	 */
 	private	function setup_shortcode() {
 	
 		add_shortcode( 'jssorslider', array( $this, 'register_shortcode' ) );
+		
 	}
 	
 	
 	/**
-	* Hook Jssor Slider into WordPress
-	*/
+	 * Hook Jssor Slider into WordPress
+	 */
 	private	function setup_actions() {
 		
 		add_action( 'admin_menu', array( $this, 'create_global_menus_for_js_slider' ) );
 		add_action( 'media_buttons_context', array( $this, 'insert_jssorslider_button' ) );
 		add_action( 'admin_footer', array( $this, 'jssorslider_admin_footer') );
+		
 	}
 	
 	
 	/**
-	*Add the menu page
-	*/
+	 *Add the menu page
+	 */
 	public function	 create_global_menus_for_js_slider() {
 		
 		$menu = array();
@@ -87,76 +93,91 @@ class JssorSliderPlugin {
 	}
 	
 	public function	load_prev1_js() {
+		
 		/* Too early to enqueue script,register against the proper action */	
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_prev1_js') );
+		
 	}
 	
 	public function	load_prev2_js() {
+		
 		/* Too early to	enqueue	script,register	against	the proper action */	
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_prev2_js' ) );
 	}
 	
 	public function	load_admin_scripts() {
+		
 		/* Too early to enqueue	script,register	against	the proper action */
 		add_action( 'admin_enqueue_scripts', array( $this, 'register_admin_scripts' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_css_calls' ) );
 	}
 	
 	/**
-	* Functions for	replacing table	names
-	*/
+	 * Functions for	replacing table	names
+	 */
 	public static function jssor_sliders() {
+		
 		global $wpdb;
 		return	$wpdb->prefix. 'jssor_sliders';
+		
 	}
 
 	public static function jssor_slides() {
+		
 		global	$wpdb;
 		return	$wpdb->prefix. 'jssor_slides';
+		
 	}
 	
 	/**
-	* Functions for	creating admin menus
-	*/
+	 * Functions for	creating admin menus
+	 */
 	public function	jssor_slider() {
 		
 		include_once JSSOR_SL_PLUGIN_DIR . '/views/dashboard.php';
+		
 	}
 	
 	public function save_slider() {
 		
 		include_once JSSOR_SL_PLUGIN_DIR . '/views/edit-slider.php';
+		
 	}
 	
 	public function slide_preview() {
 		
 		include_once JSSOR_SL_PLUGIN_DIR . '/views/slide_preview.php';
+		
 	}	
 	
 	public function caption_preview() {
+		
 		include_once JSSOR_SL_PLUGIN_DIR . '/views/caption_preview.php';
+		
 	}
 	
 	
 	/**
-	* Execute on plugin activate
-	*/
+	 * Execute on plugin activate
+	 */
 	public static function plugin_install_script() {
 		
 		include_once JSSOR_SL_PLUGIN_DIR . '/lib/install-script.php';
+		
 	}
 	
 	/**
-	* Execute on plugin uninstall
-	*/
+	 * Execute on plugin uninstall
+	 */
 	public static function plugin_uninstall_script() {
 		
 		include_once JSSOR_SL_PLUGIN_DIR . '/lib/uninstall-script.php';
+		
 	}
 	
 	/**
-	* Register admin JavaScript
-	*/
+	 * Register admin JavaScript
+	 */
 	public function	register_admin_scripts() {
 		
 		wp_enqueue_script( 'jquery-ui-core', array( 'jquery' ) );
@@ -171,8 +192,8 @@ class JssorSliderPlugin {
 	}
 	
 	/**
-	* Register admin JavaScript for	prev1_page
-	*/
+	 * Register admin JavaScript for	prev1_page
+	 */
 	public function	enqueue_prev1_js() {
 		
 		wp_enqueue_script( 'jssor.transition1.js', JSSOR_SL_PLUGIN_URL . '/assets/js/slideshow-transition.min.js' );
@@ -180,8 +201,8 @@ class JssorSliderPlugin {
 	}
 	
 	/**
-	* Register admin JavaScript for prev2_page
-	*/
+	 * Register admin JavaScript for prev2_page
+	 */
 	public function	enqueue_prev2_js() {
 	
 		wp_enqueue_script( 'jssor.transition2.js', JSSOR_SL_PLUGIN_URL . '/assets/js/caption-transition.min.js' );
@@ -189,8 +210,8 @@ class JssorSliderPlugin {
 	}
 	
 	/**
-	* Register frontend script for slider
-	*/
+	 * Register frontend script for slider
+	 */
 	public function frontend_scripts() {
 		
 		wp_enqueue_script( 'jssor.core.js', JSSOR_SL_PLUGIN_URL . '/assets/js/jssor.slider.mini.js', array( 'jquery' ), JSSORSLIDER_VERSION);
@@ -198,8 +219,8 @@ class JssorSliderPlugin {
 	}
 	
 	/**
-	* Register admin styles
-	*/
+	 * Register admin styles
+	 */
 	public function admin_css_calls() {
 
 		wp_enqueue_style( 'jquery.plupload.queue.css', JSSOR_SL_PLUGIN_URL . '/assets/css/jquery.plupload.queue.css' );
@@ -211,9 +232,10 @@ class JssorSliderPlugin {
 	}
 	
 	/**
-	* Register ajax	based function to be called on action type
-	*/
+	 * Register ajax	based function to be called on action type
+	 */
 	public function register_ajax_calls() {
+		
 		if ( isset( $_REQUEST['action'] ) ) {
 			switch ( $_REQUEST['action'] ) {
 				case 'add_new_slider_library' :
@@ -235,41 +257,42 @@ class JssorSliderPlugin {
 	}
 	
 	/**
-	* Append the 'Add Slider' button to selected admin pages
-	*/
+	 * Append the 'Add Slider' button to selected admin pages
+	 */
 	public function	insert_jssorslider_button( $context ) {
 
 		global	$pagenow;
 		
-		// Only run in post/page creation and edit screens
+		/* Only run in post/page creation and edit screens */
 		if ( in_array( $pagenow, array( 'post.php', 'page.php', 'post-new.php', 'post-edit.php' ) ) ) {
 			
-			$context .= "<a	href=\"#TB_inline?width=500&height=600&inlineId=choose-jssor-slider\"		
-			class=\"button thickbox\" title=\"" . __( "Add Slider using Jssor Slider", jssor_slider ) . "\">
-			<span style=\"background: url( ".JSSOR_SL_PLUGIN_URL."/assets/images/icon.png ) no-repeat top left;
-			display:inline-block;height:16px;margin:0 2px 0 0;vertical-align:sub;width:16px\"></span> Add Slider </a>";
+			$context .= '<a	href="#TB_inline?width=500&height=600&inlineId=choose-jssor-slider"		
+			class="button thickbox" title="' . __( "Add Slider using Jssor Slider", jssor_slider ) . '">
+			<span style="background: url( ' . JSSOR_SL_PLUGIN_URL . '/assets/images/icon.png ) no-repeat top left;
+			display:inline-block;height:16px;margin:0 2px 0 0;vertical-align:sub;width:16px"></span> Add Slider </a>';
 		}				
 		return	$context;	
 		
 	}
 
 	/**
-	* Append the 'Choose Jssor Slider' thickbox content to the bottom of selected admin pages
-	*/
+	 * Append the 'Choose Jssor Slider' thickbox content to the bottom of selected admin pages
+	 */
 	public function	jssorslider_admin_footer() {
 		
 		global	$pagenow;
 
 		/* Only	run in	post/page creation and edit screens */
 		if ( in_array( $pagenow, array( 'post.php', 'page.php', 'post-new.php', 'post-edit.php' ) ) ) {
+			
 			require_once JSSOR_SL_PLUGIN_DIR . '/front_views/jssor-slider-shortcode.php';
 		}
 	}
 
 	
 	/**
-	* Shortcode used to display slideshow
-	*/
+	 * Shortcode used to display slideshow
+	 */
 	public function	register_shortcode( $atts ) {
 		
 		if ( !isset( $atts['id'] ) ) {
@@ -284,8 +307,8 @@ class JssorSliderPlugin {
 	}
 	
 	/*
-	* @return string HTML output of	the shortcode
-	*/
+	 * @return string HTML output of	the shortcode
+	 */
 	public static function extract_shortcode( $slider_id ) {
 	
 		ob_start();
